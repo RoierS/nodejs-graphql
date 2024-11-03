1ca4662d1824f9877bf3a7d7d868c948c8094b74
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2024-11-03 17:08:45 +0100

    feat: add post/posts fields

--- src/routes/graphql/schema/RootQuery.ts
+++ src/routes/graphql/schema/RootQuery.ts
@@ -1,6 +1,8 @@
 import { GraphQLList, GraphQLNonNull, GraphQLObjectType } from 'graphql';
 import { GraphQLContext } from '../types/interfaces.js';
 import { MemberType, MemberTypeIdEnum } from '../types/MemberTypes.js';
+import { PostType } from '../types/PostType.js';
+import { UUIDType } from '../types/uuid.js';
 
 export const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
@@ -11,11 +13,31 @@ export const RootQuery = new GraphQLObjectType({
         return await context.prisma.memberType.findMany();
       },
     },
+
     memberType: {
       type: MemberType,
       args: { id: { type: new GraphQLNonNull(MemberTypeIdEnum) } },
       resolve: async (_parent, args: { id: string }, context: GraphQLContext) => {
         return await context.prisma.memberType.findUnique({
+          where: { id: args.id },
+        });
+      },
+    },
+
+    posts: {
+      type: new GraphQLList(PostType),
+      resolve: async (_parent, _args, context: GraphQLContext) => {
+        return await context.prisma.post.findMany();
+      },
+    },
+
+    post: {
+      type: PostType,
+      args: {
+        id: { type: new GraphQLNonNull(UUIDType) },
+      },
+      resolve: async (_, args: { id: string }, context: GraphQLContext) => {
+        return await context.prisma.post.findUnique({
           where: { id: args.id },
         });
       },
